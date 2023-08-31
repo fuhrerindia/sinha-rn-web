a45d68e3473cd2d1a45fc7714b6193ec63f8dc38
Paurush Sinha <paurush.sinha.d@gmail.com>
2023-08-31 15:05:37 +0530

    Initial Commit

---
+++ README.md
@@ -0,0 +1,38 @@
+# React Native Web CLI
+This is Command Line tool to create a React Native Project with `web` support. By default `react-native` only initializes `android` and `iOS` support, but this CLI tool  can be used to initialize a project with `web` support.
+
+- It uses conventional `npx react-native init` command only to initialize React Native project, and then adds packages and files required for `web` support.
+
+- You can add `web` support to existing React Native projects too with this tool.
+
+- It is not using `create-react-app` to initialize `web` platform, instead it is using `webpack` directly.
+
+- It uses `react-native-web` under the hood
+
+## Commands
+### rn-web init
+Use this command to initialize the project, it runs `npx react-native@latest init` command hence it will take few more time maybe few minutes to initialize project. It the adds support to the created project.
+
+Use command below to initialize the app
+```bash
+npx rn-web@latest init MyTestApp
+```
+### rn-web add-web
+This command adds `web` platform support to existing `React Native` projects, hence if you have an existing project then navigate to your project root and run the command below to add `web` support.
+
+```bash
+npx rn-web@latest add-web
+```
+
+## Testing and Building
+### Test
+To test the `web` platform while development, run the command below in your project.
+```bash
+npm run web
+```
+### Building
+To create production build of your project for web, run the command below.
+```bash
+npm run web-build
+```
+You will get your production build in `build` folder in your project's `web` directory. Hence in `./web/build/`
---
+++ index.js
@@ -0,0 +1,67 @@
+import chalk from "chalk";
+import Box from 'cli-box';
+import shelljs from "shelljs";
+import fs from 'fs';
+
+const ACTION = process.argv[2];
+const API_KEY = "ef2d2010-9399-4a50-b506-33bb07132ce6";
+const PROJECT_BUCKET = "rn-web-files";
+function downloadFile(serverFileName, outPutFileName) {
+    shelljs.exec(`curl -L -H "Authorization: Bearer ${API_KEY}" "https://sourceforge.net/projects/${PROJECT_BUCKET}/files/${serverFileName}" -o ${outPutFileName}`);
+}
+function modifyPackageJson(){
+    fs.readFile('package.json', 'utf8', (err, data)=>{
+        console.log('first')
+        if (err){
+            console.log(chalk.bold.red(err));
+        }else{
+            let packageData = JSON.parse(data);
+            packageData['scripts']['web'] = `webpack-dev-server --mode development --open --hot`;
+            packageData['scripts']['web-build'] = `webpack --mode production`;
+            const updatedPackage = JSON.stringify(packageData, null, 2);
+            console.log(updatedPackage)
+            fs.writeFile('package-a.json', updatedPackage, 'utf8', (err)=>{
+                if (err){
+                    console.log(chalk.bold.red("Error in Updating package.json"));
+                }else{
+                    shelljs.rm('yarn.lock');
+                    shelljs.rm('package-lock.json');
+                    shelljs.exec('npm install');
+                }
+            });
+        }
+    });
+}
+
+function addWebSupport() {
+    shelljs.exec('npm install react react-dom react-native-web');
+    shelljs.exec('npm install @babel/core @babel/preset-env @babel/preset-react babel-loader html-webpack-plugin webpack webpack-cli webpack-dev-server --save-dev');
+    shelljs.rm('App.*');
+    downloadFile('App.tsx', 'App.jsx');
+    downloadFile('webpack.config.js', 'webpack.config.js');
+    shelljs.mkdir('web');
+    shelljs.cd('web');
+    downloadFile('index.html', 'index.html');
+    downloadFile('index.js', 'index.js');
+    shelljs.cd('..');
+    modifyPackageJson();
+
+    console.log(Box({w: 50, h: 5}, chalk.bold.green('Success: Web Modules were added to the project')));
+    console.log(chalk.blue('Run `npm run web` to test on WEB'))
+
+}
+
+if (ACTION === 'init') {
+    if (process.argv[3] === undefined) {
+        console.log(chalk.bold.yellow('Error: No Project Name Provided'));
+    } else {
+        const appName = process.argv[3];
+        var b5 = Box({ w: 40, h: 3 }, chalk.bold.blue(`Creating React Native Project: ${appName}`));
+        console.log(b5.toString());
+        shelljs.exec(`npx react-native@latest init ${appName}`)
+        shelljs.cd(appName);
+        addWebSupport();
+    }
+} else if (ACTION === 'add-web') {
+    addWebSupport();
+}
